9de94a87282d66fec55499d730c9e4f1fd26a11e
spalger <spalger@users.noreply.github.com>
2016-04-04 11:11:29 -0700

    kbn_top_nav directive changes

--- src/ui/public/kbn_top_nav/kbn_top_nav.js
+++ src/ui/public/kbn_top_nav/kbn_top_nav.js
@@ -3,126 +3,83 @@ import 'ui/watch_multi';
 import angular from 'angular';
 import 'ui/directives/input_focus';
 import uiModules from 'ui/modules';
-var module = uiModules.get('kibana');
+import KbnTopNavControllerProvider from './kbn_top_nav_controller';
 
+const module = uiModules.get('kibana');
 
 /**
  * kbnTopNav directive
  *
  * The top section that shows the timepicker, load, share and save dialogues.
+ *
  * ```
  * <kbn-top-nav name="current-app-for-extensions" config="path.to.menuItems"></kbn-top-nav>
  * ```
+ *
+ * Menu items/templates are passed to the kbnTopNav via the config attribute
+ * and should be defined as an array of objects. Each object represents a menu
+ * item and should have the following properties:
+ *
+ * @param {Array<Object>|KbnTopNavController} config
+ * @param {string} config[].key
+ *        - the uniq key for this menu item.
+ * @param {string} [config[].label]
+ *        - optional, string that will be displayed for the menu button.
+ *        Defaults to the key
+ * @param {string} [config[].description]
+ *        - optional, used for the screen-reader description of this menu
+ *        item, defaults to "Toggle ${key} view" for templated menu items
+ *        and just "${key}" for programatic menu items
+ * @param {boolean} [config[].hideButton]
+ *        - optional, set to true to prevent a menu item from being created.
+ *        This allow injecting templates into the navbar that don't have
+ *        an associated template
+ * @param {function} [config[].run]
+ *        - optional, function to call when the menu item is clicked, defaults
+ *        to toggling the template
+ *
+ * Programatic control of the navbar can be acheived one of two ways
  */
-
 module.directive('kbnTopNav', function (Private) {
-  const filterTemplate = require('ui/chrome/config/filter.html');
-  const intervalTemplate = require('ui/chrome/config/interval.html');
-  function optionsNormalizer(defaultFunction, opt) {
-    if (!opt.key) {
-      return false;
-    }
-    return _.assign({
-      label: _.capitalize(opt.key),
-      hasFunction: !!opt.run,
-      description: ('Toggle ' + opt.key),
-      noButton: !!opt.noButton,
-      run: defaultFunction
-    }, opt);
-  }
-  function getTemplatesMap(configs) {
-    const templateMap = {};
-    configs.forEach(conf => {
-      if (conf.template) {
-        templateMap[conf.key] = conf.template;
-      }
-    });
-    return templateMap;
-  }
   return {
     restrict: 'E',
     transclude: true,
-    template: function ($el, $attrs) {
+    template($el, $attrs) {
       // This is ugly
       // This is necessary because of navbar-extensions
       // It will no accept any programatic way of setting its name
       // besides this because it happens so early in the digest cycle
       return `
-      <navbar class="kibana-nav-options">
-        <div ng-transclude></div>
-        <div class="button-group kibana-nav-actions" role="toolbar">
-          <button
-            ng-repeat="menuItem in kbnTopNav.menuItems"
-            aria-label="{{::menuItem.description}}"
-            aria-haspopup="{{!menuItem.hasFunction}}"
-            aria-expanded="{{kbnTopNav.is(menuItem.key)}}"
-            ng-class="{active: kbnTopNav.is(menuItem.key)}"
-            ng-click="menuItem.run(menuItem)"
-            ng-bind="menuItem.label">
-          </button>
-          <navbar-extensions name="${$attrs.name}"></navbar-extensions>
+        <navbar class="kibana-nav-options">
+          <div ng-transclude></div>
+          <div class="button-group kibana-nav-actions" role="toolbar">
+            <button
+              ng-repeat="menuItem in kbnTopNav.menuItems"
+              aria-label="{{::menuItem.description}}"
+              aria-haspopup="{{!menuItem.hasFunction}}"
+              aria-expanded="{{kbnTopNav.is(menuItem.key)}}"
+              ng-class="{active: kbnTopNav.is(menuItem.key)}"
+              ng-click="menuItem.run(menuItem)"
+              ng-bind="menuItem.label">
+            </button>
+            <navbar-extensions name="${$attrs.name}"></navbar-extensions>
+          </div>
+          <kbn-global-timepicker></kbn-global-timepicker>
+        </navbar>
+        <div class="config" ng-show="kbnTopNav.rendered">
+          <div id="template_wrapper" class="container-fluid"></div>
+          <div class="config-close remove">
+            <i class="fa fa-chevron-circle-up" ng-click="kbnTopNav.close()"></i>
+          </div>
         </div>
-        <kbn-global-timepicker></kbn-global-timepicker>
-      </navbar>
-      <div class="config" ng-show="kbnTopNav.currTemplate">
-        <div id="template_wrapper" class="container-fluid"></div>
-        <div class="config-close remove">
-          <i class="fa fa-chevron-circle-up" ng-click="kbnTopNav.close()"></i>
-        </div>
-      </div>`;
+      `;
     },
-    controller: ['$scope', '$compile', '$attrs', function ($scope, $compile, $attrs) {
-      const ctrlObj = this;
-      // toggleCurrTemplate(false) to turn it off
-      ctrlObj.toggleCurrTemplate = function (which) {
-        if (ctrlObj.curr === which || !which) {
-          ctrlObj.curr = null;
-        } else {
-          ctrlObj.curr = which;
-        }
+    controller($scope, $compile, $attrs, $element) {
+      const KbnTopNavController = Private(KbnTopNavControllerProvider);
 
-        const templateToCompile = ctrlObj.templates[ctrlObj.curr] || false;
-
-        if ($scope.kbnTopNav.currTemplate) {
-          $scope.kbnTopNav.currTemplateScope.$destroy();
-          $scope.kbnTopNav.currTemplate.remove();
-        }
-
-        if (templateToCompile) {
-          $scope.kbnTopNav.currTemplateScope = $scope.$new();
-          $scope.kbnTopNav.currTemplate = $compile(templateToCompile)($scope.kbnTopNav.currTemplateScope);
-        } else {
-          $scope.kbnTopNav.currTemplateScope = null;
-          $scope.kbnTopNav.currTemplate = false;
-        }
-      };
-      const normalizeOpts = _.partial(optionsNormalizer, (item) => {
-        ctrlObj.toggleCurrTemplate(item.key);
-      });
-
-      const niceMenuItems = _.compact(_.get($scope, $attrs.config, []).map(normalizeOpts));
-      ctrlObj.templates = _.assign({
-        interval: intervalTemplate,
-        filter: filterTemplate,
-      }, getTemplatesMap(niceMenuItems));
-
-      $scope.kbnTopNav = {
-        menuItems: niceMenuItems.filter(item => !item.noButton),
-        currTemplate: false,
-        is: which => { return ctrlObj.curr === which; },
-        close: () => { ctrlObj.toggleCurrTemplate(false); },
-        toggle: ctrlObj.toggleCurrTemplate,
-        open: which => {
-          if (ctrlObj.curr !== which) {
-            ctrlObj.toggleCurrTemplate(which);
-          }
-        }
-      };
-    }],
-    link: function ($scope, element, attr, configCtrl) {
-      $scope.$watch('kbnTopNav.currTemplate', newVal => {
-        element.find('#template_wrapper').html(newVal);
-      });
+      $scope.kbnTopNav = new KbnTopNavController(_.get($scope, $attrs.config));
+      $scope.kbnTopNav._link($scope, $element);
+      return $scope.kbnTopNav;
     }
   };
 });
