1da22f9046cf6d459229ae0ef301513790208a10
spalger <spalger@users.noreply.github.com>
2016-04-17 18:12:42 -0700

    [test/functional/support] remove IIFE wrappers in pages

--- test/support/pages/header_page.js
+++ test/support/pages/header_page.js
@@ -1,137 +1,134 @@
 import { common, remote, defaultTimeout } from '../';
 
-export default (function () {
+// the page object is created as a constructor
+// so we can provide the remote Command object
+// at runtime
+function HeaderPage() {
+  this.remote = remote;
+}
 
-  // the page object is created as a constructor
-  // so we can provide the remote Command object
-  // at runtime
-  function HeaderPage() {
-    this.remote = remote;
+HeaderPage.prototype = {
+  constructor: HeaderPage,
+
+  clickSelector: function (selector) {
+    var self = this.remote;
+    return common.tryForTime(defaultTimeout, function () {
+      return self.setFindTimeout(defaultTimeout)
+      .findByCssSelector(selector)
+      .then(function (tab) {
+        return tab.click();
+      });
+    });
+  },
+
+  clickDiscover: function () {
+    common.debug('click Discover tab');
+    this.clickSelector('a[href*=\'discover\']');
+  },
+
+  clickVisualize: function () {
+    common.debug('click Visualize tab');
+    this.clickSelector('a[href*=\'visualize\']');
+  },
+
+  clickDashboard: function () {
+    common.debug('click Dashboard tab');
+    this.clickSelector('a[href*=\'dashboard\']');
+  },
+
+  clickSettings: function () {
+    common.debug('click Settings tab');
+    this.clickSelector('a[href*=\'settings\']');
+  },
+
+  clickTimepicker: function clickTimepicker() {
+    return this.remote.setFindTimeout(defaultTimeout)
+    .findDisplayedByClassName('navbar-timepicker-time-desc').click();
+  },
+
+  clickAbsoluteButton: function clickAbsoluteButton() {
+    return this.remote.setFindTimeout(defaultTimeout)
+    .findByLinkText('Absolute').click();
+  },
+
+  setFromTime: function setFromTime(timeString) {
+    return this.remote.setFindTimeout(defaultTimeout)
+    .findByCssSelector('input[ng-model=\'absolute.from\']')
+    .clearValue()
+    .type(timeString);
+  },
+
+  setToTime: function setToTime(timeString) {
+    return this.remote.setFindTimeout(defaultTimeout)
+    .findByCssSelector('input[ng-model=\'absolute.to\']')
+    .clearValue()
+    .type(timeString);
+  },
+
+  clickGoButton: function clickGoButton() {
+    return this.remote.setFindTimeout(defaultTimeout)
+    .findByClassName('kbn-timepicker-go')
+    .click();
+  },
+
+
+  setAbsoluteRange: function setAbsoluteRange(fromTime, toTime) {
+    var self = this;
+    common.debug('clickTimepicker');
+    return self.clickTimepicker()
+    .then(function () {
+      common.debug('--Clicking Absolute button');
+      return self.clickAbsoluteButton();
+    })
+    .then(function () {
+      common.debug('--Setting From Time : ' + fromTime);
+      return self.setFromTime(fromTime);
+    })
+    .then(function () {
+      common.debug('--Setting To Time : ' + toTime);
+      return self.setToTime(toTime);
+    })
+    .then(function () {
+      return self.clickGoButton();
+    })
+    .then(function () {
+      return self.collapseTimepicker();
+    });
+  },
+
+  collapseTimepicker: function collapseTimepicker() {
+    return this.remote.setFindTimeout(defaultTimeout)
+    .findByCssSelector('.fa.fa-chevron-circle-up')
+    .click();
+  },
+
+  getToastMessage: function getToastMessage() {
+    return this.remote.setFindTimeout(defaultTimeout)
+    .findDisplayedByCssSelector('kbn-truncated.toast-message.ng-isolate-scope')
+    .getVisibleText();
+  },
+
+  waitForToastMessageGone: function waitForToastMessageGone() {
+    var self = this;
+
+    return self.remote.setFindTimeout(defaultTimeout)
+      .waitForDeletedByCssSelector('kbn-truncated.toast-message');
+  },
+
+  clickToastOK: function clickToastOK() {
+    return this.remote
+    .setFindTimeout(defaultTimeout)
+    .findByCssSelector('button[ng-if="notif.accept"]')
+    .click();
+  },
+
+  getSpinnerDone: function getSpinnerDone() {
+    var self = this;
+    return this.remote
+    .setFindTimeout(defaultTimeout * 10)
+    .findByCssSelector('.spinner.ng-hide');
   }
 
-  HeaderPage.prototype = {
-    constructor: HeaderPage,
+};
 
-    clickSelector: function (selector) {
-      var self = this.remote;
-      return common.tryForTime(defaultTimeout, function () {
-        return self.setFindTimeout(defaultTimeout)
-        .findByCssSelector(selector)
-        .then(function (tab) {
-          return tab.click();
-        });
-      });
-    },
-
-    clickDiscover: function () {
-      common.debug('click Discover tab');
-      this.clickSelector('a[href*=\'discover\']');
-    },
-
-    clickVisualize: function () {
-      common.debug('click Visualize tab');
-      this.clickSelector('a[href*=\'visualize\']');
-    },
-
-    clickDashboard: function () {
-      common.debug('click Dashboard tab');
-      this.clickSelector('a[href*=\'dashboard\']');
-    },
-
-    clickSettings: function () {
-      common.debug('click Settings tab');
-      this.clickSelector('a[href*=\'settings\']');
-    },
-
-    clickTimepicker: function clickTimepicker() {
-      return this.remote.setFindTimeout(defaultTimeout)
-      .findDisplayedByClassName('navbar-timepicker-time-desc').click();
-    },
-
-    clickAbsoluteButton: function clickAbsoluteButton() {
-      return this.remote.setFindTimeout(defaultTimeout)
-      .findByLinkText('Absolute').click();
-    },
-
-    setFromTime: function setFromTime(timeString) {
-      return this.remote.setFindTimeout(defaultTimeout)
-      .findByCssSelector('input[ng-model=\'absolute.from\']')
-      .clearValue()
-      .type(timeString);
-    },
-
-    setToTime: function setToTime(timeString) {
-      return this.remote.setFindTimeout(defaultTimeout)
-      .findByCssSelector('input[ng-model=\'absolute.to\']')
-      .clearValue()
-      .type(timeString);
-    },
-
-    clickGoButton: function clickGoButton() {
-      return this.remote.setFindTimeout(defaultTimeout)
-      .findByClassName('kbn-timepicker-go')
-      .click();
-    },
-
-
-    setAbsoluteRange: function setAbsoluteRange(fromTime, toTime) {
-      var self = this;
-      common.debug('clickTimepicker');
-      return self.clickTimepicker()
-      .then(function () {
-        common.debug('--Clicking Absolute button');
-        return self.clickAbsoluteButton();
-      })
-      .then(function () {
-        common.debug('--Setting From Time : ' + fromTime);
-        return self.setFromTime(fromTime);
-      })
-      .then(function () {
-        common.debug('--Setting To Time : ' + toTime);
-        return self.setToTime(toTime);
-      })
-      .then(function () {
-        return self.clickGoButton();
-      })
-      .then(function () {
-        return self.collapseTimepicker();
-      });
-    },
-
-    collapseTimepicker: function collapseTimepicker() {
-      return this.remote.setFindTimeout(defaultTimeout)
-      .findByCssSelector('.fa.fa-chevron-circle-up')
-      .click();
-    },
-
-    getToastMessage: function getToastMessage() {
-      return this.remote.setFindTimeout(defaultTimeout)
-      .findDisplayedByCssSelector('kbn-truncated.toast-message.ng-isolate-scope')
-      .getVisibleText();
-    },
-
-    waitForToastMessageGone: function waitForToastMessageGone() {
-      var self = this;
-
-      return self.remote.setFindTimeout(defaultTimeout)
-        .waitForDeletedByCssSelector('kbn-truncated.toast-message');
-    },
-
-    clickToastOK: function clickToastOK() {
-      return this.remote
-      .setFindTimeout(defaultTimeout)
-      .findByCssSelector('button[ng-if="notif.accept"]')
-      .click();
-    },
-
-    getSpinnerDone: function getSpinnerDone() {
-      var self = this;
-      return this.remote
-      .setFindTimeout(defaultTimeout * 10)
-      .findByCssSelector('.spinner.ng-hide');
-    }
-
-  };
-
-  return HeaderPage;
-}());
+module.exports = HeaderPage;
